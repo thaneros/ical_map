8c88294f9d63a95d61f2336590f7d678f9ddc9d8
benjamin <benjamin@codegreenit.com>
2013-02-22 17:39:30 +0800

    Added calendar test file

--- test/test_ical_map.rb
+++ test/test_ical_map.rb
@@ -3,6 +3,6 @@ require 'ical_map'
 
 class IcalMapTest < Test::Unit::TestCase
   def test_read_calendars_from_file
-    assert (IcalMap::IcalUtils.getCalendarsFromFile "/home/benjamin/Downloads/TerrificTerrariums.ics").first.class.eql? Icalendar::Calendar
+    assert (IcalMap::IcalUtils.getCalendarsFromFile "test/TerrificTerrariums.ics").first.class.eql? Icalendar::Calendar
   end
 end
