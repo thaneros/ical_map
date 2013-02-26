b9a44c1e7142080b1ba174bab96d57545207fb10
benjamin <benjamin@codegreenit.com>
2013-02-26 14:55:20 +0800

    Changed test calendar file

--- lib/ical_map/event.rb
+++ lib/ical_map/event.rb
@@ -11,6 +11,7 @@ class IcalMap::Event
   end
 
   def get_lat_lng_from_location (location)
+    location_found = false
     RestClient.get("http://maps.googleapis.com/maps/api/geocode/json", {:params =>  {:address => location, :sensor => "false"}}) do |response, request, result, &block|
       case response.code
       when 200
@@ -18,9 +19,11 @@ class IcalMap::Event
         if resh["status"] == "OK"
           @lat = resh["results"][0]["geometry"]["location"]["lat"]
           @lng = resh["results"][0]["geometry"]["location"]["lng"]
+          location_found = true
         end
       end
     end
+    return location_found
   end
 end
 
