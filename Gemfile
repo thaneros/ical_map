e3a828915853da26697f2d948f779e12bc98218c
benjamin <benjamin@codegreenit.com>
2013-04-09 17:54:49 +0800

    Cleaned the tests

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem "icalendar"
+gem "icalendar", git: "https://github.com/icalendar/icalendar.git"
 gem 'rest-client'
 gem 'json'
 gem "rake"
--- test/ical_map_test.rb
+++ test/ical_map_test.rb
@@ -1,11 +1,9 @@
 require 'test/unit'
 require 'ical_map'
-require 'date'
-require 'icalendar'
 
 class IcalMapTest < Test::Unit::TestCase
   def test_read_calendars_from_file
-    assert ((IcalMap::IcalUtils.get_calendars_from_file "test/TerrificTerrariums.ics").first.class.eql? Icalendar::Calendar), "The object read should be a Calendar but is not one"
+    assert ((IcalMap::IcalUtils.get_calendars_from_file "test/fixtures/TerrificTerrariums.ics").first.class.eql? Icalendar::Calendar), "The object read should be a Calendar but is not one"
   end
 
   def test_assign_calendar_event_from_calendar
@@ -17,7 +15,7 @@ class IcalMapTest < Test::Unit::TestCase
       description "Have a long lunch meeting and decide nothing..."
       location    "Hawaii Nature Center - Oahu - 2131 Makiki Heights Drive , Honolulu, HI US 96822"
     end
-    cal.publish 
+    cal.publish
     my_event = IcalMap::Event.new cal.events.first
     assert_equal cal.events.first.summary, my_event.title
     assert_equal cal.events.first.description, my_event.description
@@ -29,12 +27,11 @@ class IcalMapTest < Test::Unit::TestCase
   end
 
   def test_assign_multiple_calendar_events_from_file
-    p IcalMap::IcalUtils.get_events_from_file "test/multipleEventsCalendar.ics"
-    assert ((IcalMap::IcalUtils.get_events_from_file "test/multipleEventsCalendar.ics").first.class.eql? IcalMap::Event), "The object read should be a Event but is not one"
+    assert ((IcalMap::IcalUtils.get_events_from_file "test/fixtures/MultipleEventsCalendar.ics").first.class.eql? IcalMap::Event), "The object read should be a Event but is not one"
   end
 
   def test_lat_lng_are_retrieved_from_location
-    event = (IcalMap::IcalUtils.get_events_from_file "test/TerrificTerrariums.ics").first
+    event = (IcalMap::IcalUtils.get_events_from_file "test/fixtures/TerrificTerrariums.ics").first
     assert_equal 21.3131117, event.lat
     assert_equal -157.8294964, event.lng
   end
